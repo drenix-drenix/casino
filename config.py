09c8e7993820ae15585121c23868fda998728cf5
drenix-drenix <109510841+drenix-drenix@users.noreply.github.com>
2022-11-23 17:22:38 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+channel_id = -1001847698764  #id канала (для уведомлений)
+
+parner_channel = -1001847698764  # id канала для выплат партнерам
+
+admin = 1627807288  #id админстратора (для выплат)
+
+chats = -1001805371808 #id чата
+
+link_chat = 'https://t.me/+lVuPxgnksJw0ZDVi' #Ссылка на чат
+
+token = '5926955950:AAHgVACS3wUUp3zEsLMZQGMu-qH5LvR8R9Y' #токен бота
+
+username_bot = '@xcasino_x_bot'
+
+news_link = 'https://t.me/+QTvuOoKy40c2NmQy'
+
+admin_link = 'https://t.me/drenix_x'
